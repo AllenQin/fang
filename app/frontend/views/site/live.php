606e9e03dccdc508c2310d741e06d522f516496a
秦海龙 <qinhailong@mengzhu.live>
2017-01-06 15:02:29 +0800

    add player

--- app/frontend/views/site/live.php
+++ app/frontend/views/site/live.php
@@ -16,5 +16,5 @@ $this->params['breadcrumbs'][] = $this->title;
 	</p> <small>关键词 <cite>开源</cite></small>
 </blockquote>
 <div style="width: 640px; height: 360px; ">
-    <script type="text/javascript" src="../player/sewise.player.min.js?server=live&type=rtmp&streamurl=rtmp://219.232.161.204/livestream/mtzysunq&autostart=true&pid=&shifttime=&buffer=5&lang=en_US&logo=http://onvod.sewise.com/libs/swfplayer/skin/images/logo.png&title=LiveVideo&skin=liveWhite"></script>
+    <script type="text/javascript" src="/js/player/sewise.player.min.js?server=live&type=rtmp&streamurl=&autostart=true&pid=&shifttime=&buffer=5&lang=en_US&logo=&title=LiveVideo&skin=liveWhite"></script>
 </div>
