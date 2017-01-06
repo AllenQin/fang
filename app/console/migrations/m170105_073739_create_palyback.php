30313fcca7e6e7845240cfe477eb168e10c5efc8
秦海龙 <qinhailong@mengzhu.live>
2017-01-06 11:31:24 +0800

    update playback video address

--- app/console/migrations/m170105_073739_create_palyback.php
+++ app/console/migrations/m170105_073739_create_palyback.php
@@ -15,6 +15,7 @@ class m170105_073739_create_palyback extends Migration
             'id' => $this->primaryKey(11)->unsigned()->comment('the row primary key'),
             'uid' => $this->integer(1)->notNull()->unsigned()->comment('user id'),
             'channel_id' => $this->integer(11)->notNull()->unsigned()->comment('channel id'),
+            'url' => $this->string()->comment('video url'),
             'status' => $this->smallInteger(1)->notNull()->defaultValue(0)->unsigned()->comment('play status 0:already deleted 1:normal status'),
             'created_at' => $this->integer(1)->unsigned()->notNull()->comment('created timestamp'),
             'updated_at' => $this->integer(1)->unsigned()->notNull()->comment('updated timestamp'),
