f45ae8aca7b6b519b97805df7ee42865094f8382
秦海龙 <qinhailong@mengzhu.live>
2017-01-07 17:01:56 +0800

    更新直播代码

--- app/frontend/views/site/live.php
+++ app/frontend/views/site/live.php
@@ -15,6 +15,27 @@ $this->params['breadcrumbs'][] = $this->title;
 		测试小区直播.
 	</p> <small>关键词 <cite>朝阳</cite><cite>CBD</cite></small>
 </blockquote>
+<link rel="stylesheet" href="http://g.alicdn.com/de/prismplayer/1.4.7/skins/default/index.css" />
+<script type="text/javascript" src="http://g.alicdn.com/de/prismplayer/1.4.7/prism.js"></script>
+
+  
+<script src="/js/flv.min.js"></script>
+<video id="videoElement"></video>
+<script>
+if (flvjs.isSupported()) {
+    var videoElement = document.getElementById('videoElement');
+    var flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        url: 'http://live.allenqin.com/fang/20170107_sd.flv'
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    flvPlayer.play();
+}
+</script>
+
+<!--
 <div style="width: auto; height: 360px; ">
     <script type="text/javascript" src="/js/player/sewise.player.min.js?server=live&type=flv&streamurl=<?php echo $rtmpurl.$streamurl;?>&autostart=true&pid=&shifttime=&buffer=3&lang=en_US&logo=&title=LiveVideo&skin=liveWhite"></script>
 </div>
+-->
