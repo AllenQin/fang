ec1effa3549e8f130ec9c519c27819f449160c4a
秦海龙 <qinhailong@mengzhu.live>
2017-01-07 17:39:05 +0800

    更新直播代码

--- app/frontend/views/site/live.php
+++ app/frontend/views/site/live.php
@@ -15,15 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 		测试小区直播.
 	</p> <small>关键词 <cite>朝阳</cite><cite>CBD</cite></small>
 </blockquote>
-<link href="http://vjs.zencdn.net/5.5.3/video-js.css" rel="stylesheet">
-<!-- If you'd like to support IE8 -->
-<script src="http://vjs.zencdn.net/ie8/1.1.1/videojs-ie8.min.js"></script>
-<video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
- poster="https://img.alicdn.com/imgextra/i2/754328530/TB2FpxhkXXXXXa5XXXXXXXXXXXX_!!754328530.jpg" data-setup="{}">
-    <source src="<?php echo $rtmpurl.$streamurl;?>" type="rtmp/flv">
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a web browser that
-      <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-    </p>
- </video>
- <script src="http://vjs.zencdn.net/5.5.3/video.js"></script>
+<video controls autoplay>  
+    <source src="http://live.allenqin.com/fang/20170107.m3u8" type="application/vnd.apple.mpegurl" />
+    <p class="warning">Your browser does not support HTML5 video.</p>  
+</video>
