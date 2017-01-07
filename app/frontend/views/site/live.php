463b02d65dd1a20951e4e90585e50f1e3f67b433
秦海龙 <qinhailong@mengzhu.live>
2017-01-07 17:45:36 +0800

    更新直播代码

--- app/frontend/views/site/live.php
+++ app/frontend/views/site/live.php
@@ -15,7 +15,14 @@ $this->params['breadcrumbs'][] = $this->title;
 		测试小区直播.
 	</p> <small>关键词 <cite>朝阳</cite><cite>CBD</cite></small>
 </blockquote>
-<video controls autoplay>  
+<style>
+video {
+    height: 100%;
+    width: 100%;
+    display: block;
+}
+</style>
+<video controls autoplay class="video">  
     <source src="http://live.allenqin.com/fang/20170107.m3u8" type="application/vnd.apple.mpegurl" />
     <p class="warning">Your browser does not support HTML5 video.</p>  
 </video>
