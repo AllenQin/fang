3e675f684382ef5ef32dfcfd8c6f2109c9f35494
秦海龙 <qinhailong@mengzhu.live>
2017-01-07 13:03:39 +0800

    更新直播代码

--- app/frontend/views/site/live.php
+++ app/frontend/views/site/live.php
@@ -15,6 +15,6 @@ $this->params['breadcrumbs'][] = $this->title;
 		测试小区直播.
 	</p> <small>关键词 <cite>朝阳</cite><cite>CBD</cite></small>
 </blockquote>
-<div style="width: 640px; height: 360px; "azimuth: >
+<div style="width: auto; height: 360px; ">
     <script type="text/javascript" src="/js/player/sewise.player.min.js?server=live&type=flv&streamurl=<?php echo $rtmpurl.$streamurl;?>&autostart=true&pid=&shifttime=&buffer=3&lang=en_US&logo=&title=LiveVideo&skin=liveWhite"></script>
 </div>
